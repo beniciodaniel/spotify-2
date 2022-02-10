da7521753bd5613b75f196b2baeeebf19391b4c7
Benício Barreto <benicio.daniel@gmail.com>
2022-02-09 23:01:32 -0300

    Fixing first load data by fetching user on server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import { Center, Sidebar } from '../components/organisms'
 
@@ -16,4 +18,14 @@ export default function Home() {
       <div>{/* Player */}</div>
     </div>
   )
+}
+
+// pre fetching the user on server
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+  return {
+    props: {
+      session,
+    },
+  }
 }
